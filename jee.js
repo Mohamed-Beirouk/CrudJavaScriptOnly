970362e80f7c3e2336a5f3d5eb0bfa209a83d67b
Mohamed Beirouk <mbeirouck@gmail.com>
2022-03-26 02:04:54 +0000

    gh

--- jee.js
+++ jee.js
@@ -1,14 +1,35 @@
-var x = [2, 5, 6, 9 ]; 
-call_me(x);
 
-function call_me(array1) {
-
-  const map1 = array1.map(x => x + (x-1));
-  console.log(map1);
-  
-}
 
 
 // TP JS: table : last name first name gender date of birth actions.
 // actions(update (nouveau form),delete(demande de confirmation !!!)).
 // trier par tout.
+let arrayetudiants = document.getElementById("MaTable");
+
+let etudiants = [
+    {
+        nom: "Mohamed",
+        prenom: "Beirouk", 
+        genre: "Homme",
+        dtNaiss: "08/08/1998",
+    },
+    {
+        nom: "medos",
+        prenom: "le goss", 
+        genre: "Homme",
+        dtNaiss: "08/08/2000",
+    },
+    {
+        nom: "meimouna",
+        prenom: "Beirouk", 
+        genre: "Femme",
+        dtNaiss: "10/10/2001",
+    },
+    {
+        nom: "ramla",
+        prenom: "Beirouk", 
+        genre: "Femme",
+        dtNaiss: "15/08/2005",
+    },
+    
+  ];
