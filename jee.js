46d129c46dc0adc9490fc28028b8e26336041c42
Mohamed Beirouk <mbeirouck@gmail.com>
2022-03-26 17:42:12 +0000

    array js to html table

--- jee.js
+++ jee.js
@@ -1,35 +1,53 @@
-
-
-
 // TP JS: table : last name first name gender date of birth actions.
 // actions(update (nouveau form),delete(demande de confirmation !!!)).
 // trier par tout.
-let arrayetudiants = document.getElementById("MaTable");
 
-let etudiants = [
+
+var etudiants = [
     {
-        nom: "Mohamed",
-        prenom: "Beirouk", 
-        genre: "Homme",
-        dtNaiss: "08/08/1998",
+        'nom': "Mohamed",
+        'prenom': "Beirouk", 
+        'genre': "Homme",
+        'dtNaiss': "08/08/1998",
     },
     {
-        nom: "medos",
-        prenom: "le goss", 
-        genre: "Homme",
-        dtNaiss: "08/08/2000",
+        'nom': "medos",
+        'prenom': "le goss", 
+        'genre': "Homme",
+        'dtNaiss': "08/08/2000",
     },
     {
-        nom: "meimouna",
-        prenom: "Beirouk", 
-        genre: "Femme",
-        dtNaiss: "10/10/2001",
+        'nom': "meimouna",
+        'prenom': "Beirouk", 
+        'genre': "Femme",
+        'dtNaiss': "10/10/2001",
     },
     {
-        nom: "ramla",
-        prenom: "Beirouk", 
-        genre: "Femme",
-        dtNaiss: "15/08/2005",
+        'nom': "ramla",
+        'prenom': "Beirouk", 
+        'genre': "Femme",
+        'dtNaiss': "15/08/2005",
     },
     
   ];
+  function fetch1() {
+    
+    for (var i in etudiants) {
+       var ligne = `<tr>
+            <td> ${etudiants[i].nom}</td>
+            <td> ${etudiants[i].prenom}</td>
+            <td> ${etudiants[i].genre}</td>
+            <td> ${etudiants[i].dtNaiss}</td>
+        </tr>`
+        var table = $('#t1body')
+        table.append(ligne)
+    }
+}   
+
+$( window ).on('load',function() {
+    fetch1();
+
+});
+
+
+
